e1ae24729c87977ee59bd33006df5fdfb3c57bd3
Rashad Abdul-Salaam <rashad@getskinclick.com>
2017-08-26 14:40:48 -0400

    Additional files

---
+++ src/pages/add-place/add-place.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from "@angular/forms";
+
+
+@IonicPage()
+@Component({
+  selector: 'page-add-place',
+  templateUrl: 'add-place.html',
+})
+export class AddPlacePage {
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+
+}
